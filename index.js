2f55e58c06aaae702c3ce4be9809f54772c8edb2
NBTrong <trong.nb121001@gmail.com>
2023-10-25 20:51:00 +0700

    feature: Api CRUD city

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import mongoose from 'mongoose';
 import authRoute from './routes/auth.js';
 import usersRoute from './routes/users.js';
 import hotelsRoute from './routes/hotels.js';
+import roomsRoute from './routes/rooms.js';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 
@@ -31,6 +32,7 @@ app.use(express.json());
 
 app.use('/api/auth', authRoute);
 app.use('/api/users', usersRoute);
+app.use('/api/rooms', roomsRoute);
 app.use('/api/hotels', hotelsRoute);
 
 app.use((err, req, res, next) => {
