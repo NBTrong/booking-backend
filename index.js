98bc9078d2269be415375a7e7759ba45f50bc5db
NBTrong <trong.nb121001@gmail.com>
2023-11-02 13:35:00 +0700

    feature: Api CRUD rooms

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import authRoute from './routes/auth.js';
 import usersRoute from './routes/users.js';
 import hotelsRoute from './routes/hotels.js';
 import roomsRoute from './routes/rooms.js';
+import citiesRoute from './routes/cities.js';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 
@@ -34,6 +35,7 @@ app.use('/api/auth', authRoute);
 app.use('/api/users', usersRoute);
 app.use('/api/rooms', roomsRoute);
 app.use('/api/hotels', hotelsRoute);
+app.use('/api/cities', citiesRoute);
 
 app.use((err, req, res, next) => {
   const errorStatus = err.status || 500;
